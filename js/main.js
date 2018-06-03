e024eff55048d5ed926f8bde1a57ec9c367c26bf
Tim Caldwell <tim.caldwell@einti.com>
2018-06-03 16:59:03 +1000

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "Queen";
+var cardFour = "King";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
