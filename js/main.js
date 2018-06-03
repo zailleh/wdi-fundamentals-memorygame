bc7dd7cab6a4a7c9afc24b7c12300864969d89a0
Tim Caldwell <tim.caldwell@einti.com>
2018-06-04 09:16:10 +1000

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 console.log("Up and running!");
 
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "Queen";
-var cardFour = "King";
+
+var cards = ['Queen','Queen','King','King'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!");
+	}
+	else
+	{
+		alert("Sorry, try again");
+	}
+}
