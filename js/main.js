3686cbdc9da66cdc7acf29487425861f3cd0440f
Tim Caldwell <tim.caldwell@einti.com>
2018-06-04 10:10:42 +1000

    Unit 9 - moved code into functions

--- js/main.js
+++ js/main.js
@@ -5,26 +5,31 @@ var cards = ['Queen','Queen','King','King'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2)
-{
-	if (cardsInPlay[0] === cardsInPlay[1])
+var flipCard = function (cardID)
 	{
-		alert("You found a match!");
+		console.log("User flipped " + cards[cardID]);
+
+		cardsInPlay.push(cards[cardID]);
+
+		checkForMatch();
+
 	}
-	else
+
+var checkForMatch = function()
 	{
-		alert("Sorry, try again");
+
+		if (cardsInPlay.length === 2)
+		{
+			if (cardsInPlay[0] === cardsInPlay[1])
+			{
+				alert("You found a match!");
+			}
+			else
+			{
+				alert("Sorry, try again");
+			}
+		}
 	}
-}
+
+flipCard(0);
+flipCard(2);
